21701c106b757adfff357e5a17da2302bcebf933
Micha230 <michelli368@gmail.com>
2023-08-23 18:03:19 +0300

    update

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -100,7 +100,7 @@ const Nav = () => {
                   My Profile
                 </Link>
                 <Link
-                  href="/create-promt"
+                  href="/create-prompt"
                   className="dropdown_link"
                   onClick={() => setToggleDropdown(false)}
                 >
